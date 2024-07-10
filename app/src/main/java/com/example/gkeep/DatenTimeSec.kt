ffeb56952eb8ca6759dde2c9bafc244a5487a6cb
Sagnik Mukherjee <mukherjee.sagnik17@gmail.com>
2024-07-10 21:05:49 +0530

    basicStr

--- app/src/main/java/com/example/gkeep/DatenTimeSec.kt
+++ app/src/main/java/com/example/gkeep/DatenTimeSec.kt
@@ -5,8 +5,20 @@ import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Create
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.materialIcon
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -16,13 +28,16 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.delay
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
-
+@Preview
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun DatenTimeSec(){
@@ -39,7 +54,6 @@ fun DatenTimeSec(){
             delay(1000)
         }
     }
-
     Row(
         modifier = Modifier
             .fillMaxWidth()
@@ -53,11 +67,36 @@ fun DatenTimeSec(){
             Text(text = "${formattedTime}", fontSize = 18.sp)
         }
         Column (
-            horizontalAlignment = Alignment.End
+            modifier = Modifier
+                .height(90.dp)
+                .padding(start = 20.dp),
+            horizontalAlignment = Alignment.End,
+            verticalArrangement = Arrangement.SpaceBetween
         ){
-            Text(text = "Todo : ${nofTodos}", fontSize = 22.sp)
-            Text(text = "Done: 1", fontSize = 18.sp)
+            Row {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(imageVector = Icons.Default.Create, contentDescription = "")
+                }
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(imageVector = Icons.Default.Menu, contentDescription = "")
+                }
+            }
+            Text(text = "6/${nofTodos}", fontSize = 18.sp, modifier = Modifier.padding(4.dp, end = 3.dp))
+            LinearProgressBarDemo()
         }
     }
 
+}
+
+
+@Composable
+fun LinearProgressBarDemo() {
+    LinearProgressIndicator(
+        progress = { 0.6f },
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(20.dp)
+            .clip(RoundedCornerShape(20.dp)),
+        color = Color.Green
+    )
 }
