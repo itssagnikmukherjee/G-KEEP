465249f4031175252029656649bc18188c6fc093
itssagnikmukherjee <mukherjee.sagnik17@gmail.com>
2024-11-08 18:15:43 +0530

    viewModel added

--- app/src/main/java/com/example/gkeep/notesViewModel.kt
+++ app/src/main/java/com/example/gkeep/notesViewModel.kt
@@ -1,9 +1,27 @@
 package com.example.gkeep
-
+import android.content.Context
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.gkeep.data.database.NotesDatabase
+import com.example.gkeep.data.database.databaseInstance
+import com.example.gkeep.data.tables.Notes
+import kotlinx.coroutines.launch
 
-class notesViewModel : ViewModel(){
+class notesViewModel(val context: Context) : ViewModel(){
 
+    var db: NotesDatabase
+    init {
+        db = databaseInstance.getDB(context)
+    }
 
+    fun upsertNote(notes: Notes){
+        viewModelScope.launch {db.dao().upsertNote(notes)}
+    }
+
+    fun deleteNote(notes: Notes){
+        viewModelScope.launch {db.dao().deleteNote(notes)}
+    }
+
+    fun getNotes() = db.dao().getNotes()
 
 }
